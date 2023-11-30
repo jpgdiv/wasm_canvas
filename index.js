1ebe68ed07ababcc45d9eb18a4e1f28b98b02c0c
jpgdiv <peter@divotion.com>
2023-11-30 11:13:13 +0100

    add count log

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 // example.
 import('./pkg')
   .catch(console.error);
+
+import { add } from './pkg';
+
+function example1() {
+  console.log(`adding 1 en 2: ${add(1,2)}`)
+}
+
+window.example1 = example1;
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 use std::f64;
 use wasm_bindgen::prelude::*;
 
+#[wasm_bindgen]
+pub fn add(a: u32, b: u32) -> u32 {
+    a + b
+}
+
 #[wasm_bindgen(start)]
 fn start() {
     let document = web_sys::window().unwrap().document().unwrap();
